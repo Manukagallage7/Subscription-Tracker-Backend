b41b2f5f5719db162f06571cfebd144fb7607795
Manukagallage7 <manukakavinda1110@gmail.com>
2025-12-25 20:52:48 +0530

    feat: implement initial Express server setup with routing and middleware

---
+++ app.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import { connectDB } from './database/mongodb.js';
+import { PORT} from './config/env.js'
+import cookieParser from 'cookie-parser';
+import errorMiddleware from './middleware/errorMiddleware.js'
+import userRouter from './routes/userRoutes.js'
+import authRouter from './routes/authRoutes.js'
+import subscriptionRouter from './routes/subscriptionRoutes.js'
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cookieParser())
+
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/user', userRouter)
+app.use('/api/v1/subscriptions', subscriptionRouter)
+
+app.use(errorMiddleware)
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Subscription Tracker API');
+});
+
+app.listen(PORT, async () => {
+    console.log(`Subscription Tracker is running on port ${PORT}`)
+
+    await connectDB()
+});
