588921dbf1677a9680e3b2ec75cc0c723468645e
Manukagallage7 <manukakavinda1110@gmail.com>
2025-12-25 20:53:41 +0530

    feat: create user model with validation and schema definition

---
+++ models/userModel.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [
+            true,
+            'User Name is required'
+        ],
+        trim: true,
+        minLength: 2,
+        maxLength: 50
+    },
+    email: {
+        type: String,
+        required: [
+            true,
+            'User Email is required'
+        ],
+        trim: true,
+        lowercase: true,
+        match: [
+            /\S+@\s+\.\S+/,
+            'lese fill a valid email address']
+    },
+    password: {
+        type: String,
+        required: [
+            true,
+            'User Password is required'
+        ],
+        minLength: 6
+    }
+}, {timestamp: true})
+
+const User = mongoose.model('User', userSchema)
+
+export default User;
