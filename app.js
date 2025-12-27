1b041da32e5ab98e3fc5fa70f671b5816df1bbce
Manukagallage7 <manukakavinda1110@gmail.com>
2025-12-27 21:06:12 +0530

    feat: add arcjet middleware and workflow routes to the application

--- app.js
+++ app.js
@@ -6,16 +6,20 @@ import errorMiddleware from './middleware/errorMiddleware.js'
 import userRouter from './routes/userRoutes.js'
 import authRouter from './routes/authRoutes.js'
 import subscriptionRouter from './routes/subscriptionRoutes.js'
+import arcjetMiddleware from './middleware/arcjetMiddleware.js'
+import workflowRouter from './routes/workflowRoutes.js';
 
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cookieParser())
+app.use(arcjetMiddleware)
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/user', userRouter)
 app.use('/api/v1/subscriptions', subscriptionRouter)
+app.use('/api/v1/workflows', workflowRouter)
 
 app.use(errorMiddleware)
 
